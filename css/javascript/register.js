f6ed716e1d9b74801d9f9584dccc2bc6dc3f2d37
Matthew Allison <mattymallison@gmail.com>
2021-08-30 14:59:53 +0200

    new code

---
+++ css/javascript/register.js
@@ -0,0 +1,22 @@
+// https://sneakeromatic-api.herokuapp.com/
+function registerUser() {
+    let registerName = document.getElementById('name').value
+    let registerEmail =  document.getElementById('email').value
+    let registerPassword = document.getElementById('password').value
+    // console.log(registerName, registerEmail, registerPassword)
+    fetch('https://sneakeromatic-api.herokuapp.com/sign-up/', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: registerName,
+            email: registerEmail,
+            password: registerPassword 
+        }),
+        headers: {
+            "Content-type": "application/json",
+        },
+    })
+    .then((res) => res.json())
+    .then(data => {
+        window.location.replace("./login.html")
+    })
+}
