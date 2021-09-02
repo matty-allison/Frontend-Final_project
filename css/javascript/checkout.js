b9200708d1846cc9e04018a04d7749335490956f
Matthew Allison <mattymallison@gmail.com>
2021-09-02 10:08:15 +0200

    new code

--- css/javascript/checkout.js
+++ css/javascript/checkout.js
@@ -32,11 +32,65 @@ else{
 function removeSneaker(id) {
     let cart = []
     let restOfSneaker = sneakers.filter(sneaker => sneaker[0] != id)
-    console.log(restOfSneaker);
     localStorage.setItem('cart', JSON.stringify(restOfSneaker))
     cart = JSON.parse(localStorage.getItem('cart'))
     let newPrice = cart.reduce((total, c) => total + parseInt(c[5]), 0)
     localStorage.setItem('price', newPrice)
     document.querySelector('.totalPrice').innerHTML = 'R'+newPrice
     window.location.reload()
+}
+
+// user pick up or delivery function
+
+function collection() {
+    let container = document.querySelector('.collection-or-delivery')
+    let cart = JSON.parse(localStorage.getItem('cart'))
+    if (cart == null) {
+        container.innerHTML = 'Please select a item to purchase'
+    }
+    else if (cart == []) {
+        container.innerHTML = 'Please select a item to purchase'
+    }
+    else{
+        container.innerHTML = `<div>
+        <button></button> <p>Cavendish location</p>
+        <button></button> <p>Century City location</p>
+        <button></button> <p>Blue Route location</p>
+        <button></button> <p>Bayside Mall location</p>
+    </div>`
+    }
+}
+
+function delivery() {
+    let container = document.querySelector('.collection-or-delivery')
+    let cart = JSON.parse(localStorage.getItem('cart'))
+    if (cart == null) {
+        container.innerHTML = 'Please select a item to purchase'
+    }
+    else if (cart == []) {
+        container.innerHTML = 'Please select a item to purchase'
+    }
+    else{
+        container.innerHTML = `<button></button> <p>Courier Guy</p>
+        <button></button> <p>Ups</p>
+        <button></button> <p>Postnet to Postnet</p>`
+    }
+}
+
+// checkout Btn
+
+function checkoutProduct() {
+    let container = document.querySelector('.collection-or-delivery')
+    let cart = JSON.parse(localStorage.getItem('cart'))
+    if (cart == null) {
+        container.innerHTML = "You can't checkout with nothing in the cart."
+    }
+    else if (cart == []) {
+        container.innerHTML = "You can't checkout with nothing in the cart."
+    }
+    else{
+        localStorage.removeItem('cart')
+        container.innerHTML = "Thank you for your purchase"
+        window.location.replace('./index.html')
+    }
 }
